5461d36ac81f1623f12a5ab8e26b27fd748919e8
JoseD <95452898+JoseD2021@users.noreply.github.com>
2023-05-26 21:09:43 -0500

    Delete script.js

--- IS Covid/script.js
+++
@@ -1,5 +0,0 @@
-toggle = (i) => i.classList.toggle("summaryHide");
-window.onload = () => {
-    for (let i of document.querySelectorAll(".summary")) 
-        i.querySelector("section").addEventListener("click", ()=>toggle(i) );
-}
